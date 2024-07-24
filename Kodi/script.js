8d74de6d5b4b5e0cc497f4eb90973c13bfb19a6e
GraeyCowl <GraeyCowlCourier@proton.me>
2024-07-23 22:46:02 -0700

    Init Kodi Skin Creator Framework, start with modding Estuary (Kodi def), then does Kodi skin component library even exist?

---
+++ Kodi/script.js
@@ -0,0 +1,17 @@
+function createNewSkin() {
+  alert("Create New Skin functionality to be implemented");
+}
+
+function editLayout() {
+  alert("Edit Layout functionality to be implemented");
+}
+
+function customizeColors() {
+  alert("Customize Colors functionality to be implemented");
+}
+
+function exportSkin() {
+  alert("Export Skin functionality to be implemented");
+}
+
+// Additional JavaScript for skin creation logic will go here
